40d53347369f7377d8b318d9bb90827215ab2f35
iCodeKaty <ca.katy@icodeschool.com>
2024-06-28 15:34:26 -0500

    Delete static/keys.js

--- static/keys.js
+++
@@ -1,7 +0,0 @@
-export const hugging_face_key = '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'
-export const cloudinaryCloudname = "dmm8zr0az"
-export const sightengine_api_user = "491630136"
-export const sightengine_api_secret = "C4MoxqQPspV7jVvMv728pcy2sTpjDe9A"
-export const sightengine_workflow = "wfl_g5fdiHz4maLRX7ezhd867"
-export const sightengine_listID = "tli_g5t6wWqDfOy6QW00gUBbQ"
-export const elevenlabs_api_key = "sk_4a1c0e4c7b14d019d2da3e0c556f56c03e20e445d2242878"
